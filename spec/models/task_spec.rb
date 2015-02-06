e1df8954ef401f023a39e1803afe748d8e6c085d
katyf <katy.feng.14@gmail.com>
2015-02-06 14:07:33 -0500

    passing rspec model test for task

---
+++ spec/models/task_spec.rb
@@ -0,0 +1,21 @@
+require 'rails_helper'
+
+RSpec.describe Task do
+  describe '.create' do
+    it 'creates a new task' do
+      expect(Task.create()).to be_a Task
+    end
+
+    it 'is invalid without all params' do
+      expect(Task.create(name: 'My title')).not_to be_valid
+    end
+
+    it 'is invalid without all params' do
+      expect(Task.create(due_by: 'a time')).not_to be_valid
+    end
+
+    it 'is valid with all params' do
+      expect(Task.create(name: 'Something to do', due_by: 'sometime', est_time: 'num hours', status: 'complete')).to be_valid
+    end
+  end
+end

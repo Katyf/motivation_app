79c703ae4e56854122ca2a866712e4d04a9688fb
katyf <katy.feng.14@gmail.com>
2015-02-06 11:09:02 -0500

    Added Tasks controller, model and basic root homepage

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,67 @@
+class TasksController < ApplicationController
+  before_action :set_task, only: [:show, :edit, :update, :destroy]
+
+  # GET /tasks
+  def index
+    @tasks = Task.all
+  end
+
+  # GET /tasks/1
+  def show
+  end
+
+  # GET /testers/new
+  def new
+    @task = Task.new
+  end
+
+  # GET /tasks/1/edit
+  def edit
+  end
+
+  # POST /tasks
+  def create
+    @task = Task.new(tester_params)
+
+    respond_to do |format|
+      if @task.save
+        format.html { redirect_to @task, notice: 'New task was successfully created.' }
+        format.json { render :show, status: :created, location: @task }
+      else
+        format.html { render :new }
+        format.json { render json: @task.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # PATCH /tasks/1
+  def update
+    respond_to do |format|
+      if @task.update(task_params)
+        format.html { redirect_to @task, notice: 'Task was successfully updated.' }
+        format.json { render :show, status: :ok, location: @task }
+      else
+        format.html { render :edit }
+        format.json { render json: @task.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # DELETE /tasks/1
+  def destroy
+    @task.destroy
+    respond_to do |format|
+      format.html { redirect_to tasks_url, notice: 'Tester was successfully destroyed.' }
+      format.json { head :no_content }
+    end
+  end
+
+  private
+    def set_task
+      @task = Task.find(params[:id])
+    end
+
+    def task_params
+      params.require(:task).permit(:name, :due_date, :status)
+    end
+end
